0d497175a6c23150d60c64be25a1912324615e73
Adil4961 <adilofficial.4961@gmail.com>
2023-02-09 12:08:23 +0530

    add 2 files html&js

---
+++ spare.js
@@ -0,0 +1,637 @@
+// alert("hey Developers")
+
+ 
+// let name = "adil khan"// name is variable//adil khan is value
+
+
+// let name = "adil khan"//string
+//console.log(name)
+
+ 
+// let mobile = 8426064961;//number
+// console.log(mobile)
+
+
+// let x =;//undefined
+
+
+// let  a = 67;
+// let b = 89;// ans = boolean (true, false)
+// console.log(a<=b)
+
+
+// const ssoId = "rjna199628027721"//ssoId and employee id are same(single line comment)
+// console.log(ssoId)
+
+
+/*
+details of the @author adil khan
+ */
+// let name = "adil khan"
+// let fatherName = "aziz ahmed"
+// let mobile = 8426064961
+// console.log(fatherName)
+
+
+
+// let x = 10 
+//  y = 20
+//  console.log(y)// without "use strict" print value of "y"
+
+// "use strict"
+// let x = 10 
+// y = 20 
+// console.log(y) //with "use strict" print error
+
+
+// let companyName = "wecode"//it is an statement //every single line code is an statement
+
+// let plotNumber = 334
+// let colony = "sanjay nagar D"// it is an multi line statement// 
+// let area = "jhotwara"
+// console.log( colony)
+
+
+// const countryName = "india";// ; semi colon ;
+// console.log(countryName)
+
+//whitespace
+// let institute = "wecode " // here whitespace matters, because value type is string
+// console.log(institute)
+
+// let mobile = 8426064961;//  here whitespace doesn`t mattter, because value type is number
+// console.log(mobile)
+
+// const accountNumber = 613504 37752//space in between numbers is not valid
+// console.log(accountNumber)
+  
+//code block
+// {
+//     let name1 = "adil khan"
+//     let bagCompany = "american tourister"
+//     let mobileCompany = "samsung"
+//     let tvCompany = "redmi"
+//     let mobile = 8058370070
+//     console.log(name1,bagCompany,mobileCompany,tvCompany// code block // code block is written in curly braces
+//         ,mobile)
+// }
+
+
+// let x = 10 
+// let y = 20 
+// console.log(x+y)// + opretor
+
+// let x = 10
+// let y = 5
+// console.log(x-y)// - opretor
+
+// let x = 20 
+// let y = 10
+// console.log(x*y) // * opretor
+
+// let x = 30 
+// let y = 4
+// console.log(x/y) // divide / opretor
+
+// let x = 4 
+// let y = 3
+// console.log(x**y) // ** qube square opretor//exponentiation
+
+// let x = 10 
+// let y = 3
+// console.log(x%y) //% modulus opretor(divison reminder)
+
+// let a = 20 
+// console.log(++a,++a)// before variable++  1 number increase hoga 
+
+// let b = 20 
+// console.log(--b,--b) //before variable -- 1 number decrease hoga 
+
+// let a = 30 
+// console.log(a++,a++)// after variable ++ vhi value print hogi pr next time print krane par ek number badhke print hoga 
+  
+// let b = 20 
+// console.log(b--,b--)// after variable -- vhi value print hogi pr next time print karne par ek number kam hoke print hoga
+
+// let x = 10 
+// let y =20
+// console.log(x>y) // greater than 
+ 
+// let x = 30
+// let y = 49
+// console.log(x<y)// less than
+
+// let x = 20 
+// let y = 12
+// console.log(x<=y)// less than equal to (boolean)
+
+// let z = 80
+// let w = 78
+// console.log(z>=w)// greater than equal to (boolean)
+
+// let x = 10 
+// let y = 20 
+// console.log(x!=y)// not equal to 
+ 
+// let x = 10 
+// let y = "10"
+// console.log(x!==y) // not equal to eqaul to check data type
+
+// let v = "19"
+// let u = 19
+// console.log(v==u)// equal to eqaul to check value
+
+// let b = 10 
+// let c = "10" 
+// console.log(b===c)// === check datatype of value
+
+// let name = "adil khan"
+// console.log(name)
+
+// let pincode = 302012
+// document.write(pincode) // html page, web page me print karega
+
+// let city = "jaipur"
+// window.alert(city)// html page,web page me popup print karega 
+
+// let a = 2
+// let b = 5   
+// console.log(a=b,a+b,a-b,a*b,b/a)
+
+// let x = 2
+// let y = 10
+// console.log(x**y,y++,y++,x++,x++)
+  
+// let a = 22
+// let b = 07
+// console.log(a%b)
+
+
+// let x = 10
+// let y = 020; 
+// y += 5
+// console.log(y)
+
+// let x = 10 
+// let y = 30
+// x=y*2
+// console.log(x)
+
+// let a =  32
+// let b = 3
+// b/=2
+// console.log(b)
+
+// let x = 89
+// let y = 3
+// y%=2
+// x%=2
+// console.log(y)
+// console.log(x)
+
+// let a = 2
+// let b = 3
+// a**=b
+// console.log(a)
+
+// let a = 10
+// let b = 20 
+// let c = 20
+// console.log(a<b&& b>=c)
+
+//  let x = 10
+// let y = 20 
+// let z = 5
+// console.log(x>y||y<=z||z>x)
+
+// let a = 10
+// let b = 20
+// let c =30
+// console.log(!a>b)
+
+
+// let x = 10 
+// let y = 20
+// if(x>=y){
+//     console.log(false)
+// // }
+// console.log()
+
+
+//1Create a variable marks and assign any number. If marks are greater than 60 then print Pass else print Fail.
+
+// let marks = 59;
+// if (marks >= 60) {
+//     console.log("Pass");
+// } else {
+//     console.log("Fail");
+// }
+
+//     Take two numbers and check if both numbers are odd or not. If yes then print Odd otherwise print Even
+
+// let num = 35;
+// let num2 = 34;
+// if (num % 2 === 1 && num2 % 2 === 1) {
+//     console.log("Odd");
+// } else {
+//     console.log("Even");
+// }
+
+//     Take 4 variables a, b, c, d and assign their values in the way mentioned below and check output: a = b b = c c = d d = a
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// let d = 40;
+// a = b; //a=20,b=20,c=30,d=40
+// b = c; //a=20,b=30,c=30,d=40
+// c = d; //a=20,b=30,c=40,d=40
+// d = a; //a=20,b=30,c=40,d=20
+// console.log(a, b, c, d); //20 30 40 20
+
+//     Print 2's table using ** operator. Use any logic but it should print 2's table.
+
+// console.log(2**1);
+// console.log(2**2);
+// console.log(2**3-2);
+// console.log(2**3);
+// console.log(2**4-6);
+// console.log(2**4-4);
+// console.log(2**4-2);
+// console.log(2**4);
+// console.log(2**4+2);
+// console.log(2**4+4);
+
+//     Check output of following:
+
+// console.log(2 + 5 _ 10 - 20 \*\* 2); // -348
+// console.log((20 / 5) _ 10 + 10); // 50
+// console.log(10 && 20 + 10 \* 10); // 120 short circuiting
+// console.log(30 > 20 && 10 + 4 < 15); //true
+// console.log((30 > 20 && 10 + 4 < 15) || 10); //true
+// console.log(30 < 20 || 10 + 5 < 15 || 10); //10 short circuiting
+// console.log(30 < 20 || (10 + 5 < 15 && 10)); //false
+
+//     Take a number and check if number is odd or not. If number is Odd then print Yes else print No
+
+// let num = 35;
+// if (num % 2 === 0) {
+//     console.log("even");
+// } else {
+//     console.log("odd");
+// }
+
+//     Take a number and check if number is divisible by 10 && 5. If yes then print Yes else print no
+
+// let num = 60;
+// if (num % 10 === 0 && num % 5 === 0) {
+//     console.log("Yes");
+// } else {
+//     console.log("No");
+// }
+
+//     Take a number and check if number * 20 makes it 100 or not. If yes the print Yes else print No
+
+// let num = 5;
+// if (num \* 20 === 100) {
+//     console.log("Yes");
+// } else {
+//     console.log("No");
+// }
+
+//     Calculate your 10th percentage and print Pass or Fail if marks are more than 70 then print Pass else print Fail
+
+// let marks = 880;
+// let totalMarks = 1000;
+// let percentage = (marks / totalMarks) \* 100;
+// if (percentage > 70) {
+//     console.log("Pass");
+// } else {
+//     console.log("Fail");
+// }
+
+//     Take your age and check if your age is greater than 21 then print Adult else print Minor
+
+// let year = 2000;
+// let today = 2023;
+// let age = today - year;
+// console.log(age);
+  
+
+// let marks = 22
+// if(marks>=80){
+//     console.log("distiction")
+// }else if(marks>=60){
+//    console.log("first devision")
+// }else if(marks>=33){
+//     console.log("second devision")
+// }else {
+//  console.log("fail")
+// }
+
+// let x = 0
+// let y = 1
+// console.log(x===y)
+
+
+
+    // let a = 120
+    // let b = 200
+    // if(a>b){
+    //     console.log("A is big ")
+    // }else{
+    //     console.log("B is big")
+    // }
+
+    // let x = 12000
+    // let y = 20000
+    // x>y?console.log("x is big"):console.log("y is big")
+
+
+    //  let a = 50
+    // let b =60;
+    // let c = 220
+    // if(a>b&&a>c){
+    //     console.log("A is big")
+    // }else if(b>c&&b >a){
+    //     console.log("B is big")
+    // }else {
+    //     console.log("C is big")
+    // }
+
+//     let a = 30;
+//     let b = 80;
+//     let c = 50;
+//     let max = a;
+// if(b>max){
+//     console.log("b is big")
+// }else if(c>max){
+//     console.log("c is big")
+// }else{
+//     console.log("a is big")
+// }  
+
+// let currentYear = 2023
+// let mohmmedBirthYear = 1992
+// let asfaqBirthYear = 1996
+// let abdulBirthYear = 1985  
+//   let mohmmedAge =(currentYear-mohmmedBirthYear)
+//   let asfaqAge =(currentYear - asfaqBirthYear)
+//  let abdulAge  =(currentYear- abdulBirthYear)
+
+//  console.log(abdulAge)
+
+// let currentYear= 2023
+// let mohmmedBirthYear = 1982
+// let asfaqBirthYear = 1976     
+// let abdulBirthYear = 1985
+// let mohmmedAge = (currentYear - mohmmedBirthYear)
+// let  asfaqAge= (currentYear - asfaqBirthYear)
+// let abdulAge = (currentYear - abdulBirthYear)
+// if(mohmmedAge>=asfaqAge&& mohmmedAge>=abdulAge){
+//     console.log("mohmmed is big")
+// }else if(asfaqAge>=mohmmedAge&&asfaqAge>=abdulAge){
+//     console.log(" asfaq is big")
+// }else{
+//     console.log("abdul is big")
+// }`
+
+
+// let shoyab = 1998
+// let devender = 2000
+// let bitu = 2004
+// let year = 2023 // year //
+// let sh = year - shoyab
+// let de = year - devender
+// let bi = year - bitu
+
+// if(sh >= de) {
+//     console.log("shoyab")
+// }
+// else if ( de >= bi )
+// {
+//     console.log("devender")
+// }
+// else 
+// {
+//     console.log("bitu")
+// // }
+
+
+// let x = "20";
+// console.log(x / 2);// type conversion
+
+
+// let year =2023
+// let rustam =2002
+// let aarif =2003
+// let bitu =2005
+
+// let a = year-rustam 
+// let b = year-aarif
+// let c = year-bitu 
+
+// if(a>=b && a>=c ){
+// console.log("rustam")
+// }
+// else if(b>=c && b>=a){
+//     console.log("aarif")
+
+// }else{
+//     console.log("bitu")
+// }
+
+
+
+// let marksAdil = 70
+// let marksSameer = 79
+// let marksAli = 88
+// let marksSher = 90      
+// if (marksAdil>marksSameer&&marksAdil>marksAli&&marksAdil>marksSher){
+//     console.log("adil is first")
+// }else if(marksSameer>marksAli&&marksSameer>marksSher&&marksSameer>marksAdil){
+//     console.log("sameer is first")
+// }else if(marksAli>marksSher&&marksAli>marksSameer&&marksAli>marksAdil){
+//     console.log(" ali is fisrt")
+// }else {
+//     console.log("sher is first")
+// }
+
+// let x = "90"
+// console.log(x/2)// automatic type conversion/implicit tpye conversion/coercion
+ 
+
+// let year = 100;
+// let person1 = 30;
+// let person2 = 80;
+// let person3 = 7;
+// let person4= 60;
+// let a = (year-person1);
+// let b = (year-person2);
+// let c = (year-person3);
+// let d = (year-person4)
+
+// let Big = a;
+// if(b>c)
+// Big = b;{
+
+// } if(c>d){
+//     Big=c
+
+// }if(d>a){
+//     Big= d
+ 
+// }console.log(Big)
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// if (!(!b>c)) {
+//   console.log("Yes");
+// } else {
+//   console.log("No");
+// }
+
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// if (!(!b < c)) {
+//   console.log("Yes");
+// } else {
+//   console.log("No");
+// // }
+
+
+
+// if (!0) {
+//     console.log("Truthy");
+//   } else {
+//     console.log("Falsy");
+//   }        
+
+
+
+////////////////////////////////////////////////////
+//EXERCISE
+///////////////////////////
+
+
+
+ //1.
+// let a = 20;
+// let b = 30;
+// let marks = 100
+// let c = marks- (a+b)
+// console.log(c)
+
+
+//2.
+//  let english = 82
+//  let science = 60
+//  let math = 70
+//  let hindi = 69
+//  let sst =90 
+//  let marks = english+science+math+hindi+sst;
+//  let totalMarks = 500;
+//  let percentage = marks/totalMarks*100
+//  console.log(percentage)
+
+//3.
+ 
+// let century = 1899;
+// let year = century/100;
+// console.log(Math.floor(year))
+
+
+//4.
+// console.log("adil"+20)
+// console.log(true+"adil")
+// console.log(30+"wecode")
+
+
+//5.
+// let fisrtQuestion1 = prompt("do you drink coffee or tea")
+
+//     if(fisrtQuestion1==="tea"){
+// alert("coffee")
+// }else if(fisrtQuestion1==="coffee"){
+//     alert("tea")
+// }
+
+
+//6.
+// let num = 88
+//   if(num%2===0&&num%4===0&&num>=84&&num<=100){
+//     console.log("even")
+//   }else{
+//     console.log("odd")
+//   }
+
+
+//7.
+// let temprature =30
+// if(temprature<=10){
+//     console.log("very cold")
+// }else if(temprature<=30&&temprature>10){
+//     console.log("cold")
+// }else{
+//     console.log("hot")
+// }
+// If both conditions are true then check if number is less than 100 or number is 84 or not.
+
+
+//8.
+// let age = prompt("what is your age")
+// if(age<18){
+//     let sorry =alert("sorry visit again")}
+// else(age>=18)
+// let openAcc= prompt("do you want to open a account")
+// let  name= prompt("what is your name")
+// let aadharNumber = prompt("what is your aadhar number")
+// let address= prompt("what is your address")
+// let number = prompt("what is your mobile number")
+// {
+//     console.log(age,)
+//     console.log(openAcc)
+//     console.log(name)
+//     console.log(aadharNumber)
+//     console.log(address)
+//     console.log(number  )
+// }
+
+//9.
+// let user = prompt("what do you want to do +-*/ ")
+// let que = Number(prompt("what is fitst value"))
+// let que1 = Number(prompt("what is second value"))
+// if(user==="+"){
+//     alert(que+que1)
+// }else if(user==="-"){
+//     alert(que-que1)
+// }else if(user==="*"){
+// alert(que*que1)
+// }else if(user==="/"){
+//     alert(que/que1)
+// }else{
+//     alert("input is invalid,please retry")
+// }
+
+
+//10.
+// let english = Number(prompt("what is your english marks"))
+// let science = Number(prompt("what is your science marks"))
+// let Math = Number(prompt("what is your science marks"))
+// let Hindi = Number(prompt("what is your hindi marks"))
+// let total = english + science + Math + Hindi 
+// alert(total / 400 * 100)
+// let to = total / 400 * 100 
+// console.log(to) 
+
+
+
+let a = 0;
+for(let b = 1;b<=100;b++)
+if(b%2===1){
+a=a+b
+}
+console.log("odd "+a)
